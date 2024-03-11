cc2a049de32adef66c61c9f8a88a62dd1699acb7
Luis Daniel <daniel.guardado@outlook.es>
2024-03-11 23:39:23 +0100

    Upload 11/03/24

--- Aplicaciones Móviles/mixee/App.js
+++ Aplicaciones Móviles/mixee/App.js
@@ -10,7 +10,7 @@ import DetailsOrderScreen from "./screens/DetailsOrderScreen/DetailsOrderScreen"
 import MenuScreen from "./screens/Menu/MenuScreen";
 
 const Stack = createStackNavigator();
-
+// TODO Hay que mejorar la lógica.
 export default function App() {
   return (
     <NavigationContainer>
